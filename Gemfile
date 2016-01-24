4d8f6582bbbce5affc25254e968b5cb6174e76b2
Georgi Knox <georgicodes@github.com>
2016-01-24 18:23:40 -0500

    added in bootstrap and view paritials for static pages

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'bootstrap-sass'
+gem 'sprockets-rails', '2.3.3'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -45,3 +47,8 @@ group :development do
   gem 'spring'
 end
 
+group :test do
+  gem 'minitest-reporters', '1.0.5'
+  gem 'mini_backtrace',     '0.1.3'
+  gem 'guard-minitest',     '2.3.1'
+end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "DIVELY app home"
+    assert_select "title", "DIVELY app"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "DIVELY help"
+    assert_select "title", "help | DIVELY app"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "DIVELY about"
+    assert_select "title", "about | DIVELY app"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "contact | DIVELY app"
   end
 
 end
