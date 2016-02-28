6d1b86cac1b5cdd25d12f7bfc4a1e4fe3a886b84
Georgi Knox <georgicodes@github.com>
2016-02-28 15:10:29 -0500

    passing user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   validates(:first_name, presence: true, length: {maximum: 50})
   validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
-  validates :password, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 
